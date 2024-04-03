a6e179bbee258311615387638705531e1013b31a
matejicdunja <dunja2002matejic@gmail.com>
2024-04-03 02:51:51 +0200

    load users from db

--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -1,8 +1,11 @@
 ﻿using ATM.Models;
 using ATM.Properties;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,23 +15,61 @@ namespace ATM.Repository
 {
     public class UserRepository
     {
+        private readonly NpgsqlConnection _connection;
         private List<User> _users;
 
         public UserRepository()
         {
+            var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            _connection = new NpgsqlConnection(connectionString);
             _users = new List<User>();
             _users = LoadUsers();
         }
 
         public List<User> LoadUsers()
         {
-            string filePath = "../../Resources/Data/Users.json";
-            if (!File.Exists(filePath))
+            //string filePath = "../../Resources/Data/Users.json";
+            //if (!File.Exists(filePath))
+            //{
+            //    throw new FileNotFoundException("JSON file not found.", filePath);
+            //}
+            //string jsonContent = File.ReadAllText(filePath);
+            //return JsonConvert.DeserializeObject<List<User>>(jsonContent);
+            _users = new List<User>();
+
+            try
             {
-                throw new FileNotFoundException("JSON file not found.", filePath);
+                _connection.Open();
+                string sql = "SELECT * FROM public.\"User\"";
+
+                using (NpgsqlCommand command = new NpgsqlCommand(sql, _connection))
+                {
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            User user = new User(
+                                Convert.ToInt64(reader["Id"]),
+                                Convert.ToString(reader["Name"]).Trim(),
+                                Convert.ToString(reader["LastName"]).Trim(),
+                                Convert.ToInt32(reader["Pin"]),
+                                Convert.ToBoolean(reader["IsAdmin"]),
+                                Convert.ToString(reader["CreditCardNumber"]).Trim(),
+                                Convert.ToInt32(reader["AttemptsNumber"]),
+                                Convert.ToBoolean(reader["IsBlocked"]),
+                                Convert.ToDouble(reader["Ballance"])
+                            );
+                            _users.Add(user);
+                        }
+                    }
+                }
             }
-            string jsonContent = File.ReadAllText(filePath);
-            return JsonConvert.DeserializeObject<List<User>>(jsonContent);
+            finally
+            {
+                _connection.Close();
+            }
+
+            return _users;
         }
 
         public List<User> LoadAllBlocked()
@@ -61,6 +102,49 @@ namespace ATM.Repository
         {
             string json = JsonConvert.SerializeObject(_users);
             File.WriteAllText("../../Resources/Data/Users.json", json);
+
+
+            //try
+            //{
+            //    _connection.Open();
+            //    using (var transaction = _connection.BeginTransaction())
+            //    {
+            //        try
+            //        {
+            //            var deleteCommand = new NpgsqlCommand("DELETE FROM public.\"User\"", _connection);
+            //            deleteCommand.ExecuteNonQuery();
+
+            //            foreach (var user in _users)
+            //            {
+            //                var insertCommand = new NpgsqlCommand(
+            //                    "INSERT INTO public.\"User\" (\"Id\", \"Name\", \"LastName\", \"Pin\", \"IsAdmin\", \"CreditCardNumber\", \"AttemptsNumber\", \"IsBlocked\", \"Balance\") " +
+            //                    "VALUES (@Id, @Name, @LastName, @Pin, @IsAdmin, @CreditCardNumber, @AttemptsNumber, @IsBlocked, @Balance)", _connection);
+            //                insertCommand.Parameters.AddWithValue("@Id", user.Id);
+            //                insertCommand.Parameters.AddWithValue("@Name", user.Name);
+            //                insertCommand.Parameters.AddWithValue("@LastName", user.LastName);
+            //                insertCommand.Parameters.AddWithValue("@Pin", user.Pin);
+            //                insertCommand.Parameters.AddWithValue("@IsAdmin", user.IsAdmin);
+            //                insertCommand.Parameters.AddWithValue("@CreditCardNumber", user.CreditCardNumber);
+            //                insertCommand.Parameters.AddWithValue("@AttemptsNumber", user.AttemptsNumber);
+            //                insertCommand.Parameters.AddWithValue("@IsBlocked", user.IsBlocked);
+            //                insertCommand.Parameters.AddWithValue("@Ballance", user.Ballance);
+
+            //                insertCommand.ExecuteNonQuery();
+            //            }
+
+            //            transaction.Commit();
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            transaction.Rollback();
+            //            throw new Exception("Error while saving users to database.", ex);
+            //        }
+            //    }
+            //}
+            //finally
+            //{
+            //    _connection.Close();
+            //}
         }
 
         public void SaveChanges(User user)
@@ -96,16 +180,6 @@ namespace ATM.Repository
                 }
             }
             return false;
-        }
-
-        public void Block(User user)
-        {
-            user.IsBlocked = true;
-        }
-
-        public void Unblock(User user)
-        {
-            user.IsBlocked = false;
         }
     }
 }
