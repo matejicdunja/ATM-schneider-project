277ccf1d0326486e2dc1eac910e940b76da176f8
matejicdunja <dunja2002matejic@gmail.com>
2024-04-03 03:20:34 +0200

    user table done

--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -28,13 +28,6 @@ namespace ATM.Repository
 
         public List<User> LoadUsers()
         {
-            //string filePath = "../../Resources/Data/Users.json";
-            //if (!File.Exists(filePath))
-            //{
-            //    throw new FileNotFoundException("JSON file not found.", filePath);
-            //}
-            //string jsonContent = File.ReadAllText(filePath);
-            //return JsonConvert.DeserializeObject<List<User>>(jsonContent);
             _users = new List<User>();
 
             try
@@ -94,80 +87,89 @@ namespace ATM.Repository
 
         public void Add(User user)
         {
-            _users.Add(user);
-            Save();
-        }
+            try
+            {
+                _connection.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand())
+                {
+                    command.Connection = _connection;
+                    command.CommandText = "INSERT INTO public.\"User\" " +
+                                          "(\"Id\", \"Name\", \"LastName\", \"Pin\", \"IsAdmin\", \"CreditCardNumber\", " +
+                                          "\"AttemptsNumber\", \"IsBlocked\", \"Ballance\") " +
+                                          "VALUES (@Id, @Name, @LastName, @Pin, @IsAdmin, @CreditCardNumber, " +
+                                          "@AttemptsNumber, @IsBlocked, @Ballance)";
+                    command.Parameters.AddWithValue("Id", user.Id);
+                    command.Parameters.AddWithValue("Name", user.Name);
+                    command.Parameters.AddWithValue("LastName", user.LastName);
+                    command.Parameters.AddWithValue("Pin", user.Pin);
+                    command.Parameters.AddWithValue("IsAdmin", user.IsAdmin);
+                    command.Parameters.AddWithValue("CreditCardNumber", user.CreditCardNumber);
+                    command.Parameters.AddWithValue("AttemptsNumber", user.AttemptsNumber);
+                    command.Parameters.AddWithValue("IsBlocked", user.IsBlocked);
+                    command.Parameters.AddWithValue("Ballance", user.Ballance);
 
-        public void Save()
-        {
-            string json = JsonConvert.SerializeObject(_users);
-            File.WriteAllText("../../Resources/Data/Users.json", json);
-
-
-            //try
-            //{
-            //    _connection.Open();
-            //    using (var transaction = _connection.BeginTransaction())
-            //    {
-            //        try
-            //        {
-            //            var deleteCommand = new NpgsqlCommand("DELETE FROM public.\"User\"", _connection);
-            //            deleteCommand.ExecuteNonQuery();
-
-            //            foreach (var user in _users)
-            //            {
-            //                var insertCommand = new NpgsqlCommand(
-            //                    "INSERT INTO public.\"User\" (\"Id\", \"Name\", \"LastName\", \"Pin\", \"IsAdmin\", \"CreditCardNumber\", \"AttemptsNumber\", \"IsBlocked\", \"Balance\") " +
-            //                    "VALUES (@Id, @Name, @LastName, @Pin, @IsAdmin, @CreditCardNumber, @AttemptsNumber, @IsBlocked, @Balance)", _connection);
-            //                insertCommand.Parameters.AddWithValue("@Id", user.Id);
-            //                insertCommand.Parameters.AddWithValue("@Name", user.Name);
-            //                insertCommand.Parameters.AddWithValue("@LastName", user.LastName);
-            //                insertCommand.Parameters.AddWithValue("@Pin", user.Pin);
-            //                insertCommand.Parameters.AddWithValue("@IsAdmin", user.IsAdmin);
-            //                insertCommand.Parameters.AddWithValue("@CreditCardNumber", user.CreditCardNumber);
-            //                insertCommand.Parameters.AddWithValue("@AttemptsNumber", user.AttemptsNumber);
-            //                insertCommand.Parameters.AddWithValue("@IsBlocked", user.IsBlocked);
-            //                insertCommand.Parameters.AddWithValue("@Ballance", user.Ballance);
-
-            //                insertCommand.ExecuteNonQuery();
-            //            }
-
-            //            transaction.Commit();
-            //        }
-            //        catch (Exception ex)
-            //        {
-            //            transaction.Rollback();
-            //            throw new Exception("Error while saving users to database.", ex);
-            //        }
-            //    }
-            //}
-            //finally
-            //{
-            //    _connection.Close();
-            //}
+                    command.ExecuteNonQuery();
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
         }
 
         public void SaveChanges(User user)
         {
-            _users = LoadUsers();
+            try
+            {
+                _connection.Open();
 
-            User existingUser = _users.FirstOrDefault(u => u.Id == user.Id);
+                using (NpgsqlCommand command = new NpgsqlCommand())
+                {
+                    command.Connection = _connection;
+                    command.CommandText = "UPDATE public.\"User\" " +
+                                          "SET \"Name\" = @Name, " +
+                                              "\"LastName\" = @LastName, " +
+                                              "\"CreditCardNumber\" = @CreditCardNumber, " +
+                                              "\"IsBlocked\" = @IsBlocked, " +
+                                              "\"Ballance\" = @Ballance " +
+                                          "WHERE \"Id\" = @Id";
+                    command.Parameters.AddWithValue("Name", user.Name);
+                    command.Parameters.AddWithValue("LastName", user.LastName);
+                    command.Parameters.AddWithValue("CreditCardNumber", user.CreditCardNumber);
+                    command.Parameters.AddWithValue("IsBlocked", user.IsBlocked);
+                    command.Parameters.AddWithValue("Ballance", user.Ballance);
+                    command.Parameters.AddWithValue("Id", user.Id);
 
-            existingUser.Name = user.Name;
-            existingUser.LastName = user.LastName;
-            existingUser.CreditCardNumber = user.CreditCardNumber;
-            existingUser.IsBlocked = user.IsBlocked;
-            existingUser.Ballance = user.Ballance;
-            Save();
+                    command.ExecuteNonQuery();
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
         }
 
         public void SaveLogInAttempts(User user, int attempts)
         {
-            _users = LoadUsers();
+            try
+            {
+                _connection.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand())
+                {
+                    command.Connection = _connection;
+                    command.CommandText = "UPDATE public.\"User\" " +
+                                          "SET \"AttemptsNumber\" = @Attempts " +
+                                          "WHERE \"Id\" = @Id";
+                    command.Parameters.AddWithValue("Attempts", attempts);
+                    command.Parameters.AddWithValue("Id", user.Id);
 
-            User existingUser = _users.FirstOrDefault(u => u.Id == user.Id);
-            existingUser.AttemptsNumber = attempts;
-            Save();
+                    command.ExecuteNonQuery();
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
         }
 
         public bool CheckForCardNumber(string cardNumber)
